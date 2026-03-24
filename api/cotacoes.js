7ec9d86675e00abc219946bd2becc20b977443ed
tiagojmbrasil-cell <tiagojmbrasil@gmail.com>
2026-03-24 09:38:10 -0300

    Update cotacoes.js

--- api/cotacoes.js
+++ api/cotacoes.js
@@ -1,7 +1,7 @@
-// v8
+// v7
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(
-  process.env.URL_SUPABASE,
+  process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
 );
 export default async function handler(req, res) {
